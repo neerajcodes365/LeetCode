8efcba4f369eac54e86c43b9452d933f79556cf3
Neeraj J <neeraj2gokul4@gmail.com>
2024-12-04 19:25:07 +0530

    Time: 1 ms (66.67%), Space: 9.9 MB (66.67%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.c
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.c
@@ -22,21 +22,3 @@ bool canMakeSubsequence(char *str1, char *str2)
 	
 	return true;
 }
-/*
-*** 
-bool canMakeSubsequence(char* str1, char* str2) {
-    int len1=strlen(str1);
-    int len2=strlen(str2);
-    if(len2>len1)return false;
-int st1=0;
-int st2=0;
-for(int i=0;i<len1;i++){
-    if(str2[st2]==str1[i] || str2[st2]==(char)(((str1[i]+1-'a')%26)+'a')){
-        st2++;
-    }
-    if(st2==len2)return true;
-}
-return false;   
-}
-***
-*/
