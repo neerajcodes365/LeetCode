0d86394f9ddb2da98c14449c9bdd838fa930aa72
Neeraj J <neeraj2gokul4@gmail.com>
2024-12-06 00:00:12 +0530

    Time: 2 ms (68%), Space: 30.6 MB (10%) - LeetHub

---
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.c
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+//Constraints:
+// The number of nodes in the tree is in the range [1, 104].
+// -1000 <= Node.val <= 1000
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void help(struct TreeNode *root, int *i, char *string) {
+    if (root == NULL) return;
+
+    *i += sprintf(&string[*i], "%d", root->val);
+
+    if (root->left || root->right) {
+        string[(*i)++] = '(';
+        help(root->left, i, string);
+        string[(*i)++] = ')';
+    }
+
+    if (root->right) {
+        string[(*i)++] = '(';
+        help(root->right, i, string);
+        string[(*i)++] = ')';
+    }
+}
+
+char* tree2str(struct TreeNode* root) {
+    char *string = (char *)calloc(100000, sizeof(char));
+    int i = 0;
+    help(root, &i, string);
+    return string;
+}
