abffa0a99ca14d9dfcdd09a7bda5ba4b97959a95
Neeraj J <neeraj2gokul4@gmail.com>
2025-05-12 23:20:42 +0530

    Time: 0 ms (100%), Space: 18.7 MB (24.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,15 +2,25 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
     
-    unordered_set<int> row;
+    unordered_set<int> row;//unordered set hash table , set redblack tree,
     unordered_set<int> col;
     for(int i=0;i<matrix.size();i++){
         for(int j=0;j<matrix[i].size();j++){
-            if(arr[i][j]==0){
+            if(matrix[i][j]==0){
                 row.insert(i);
+                col.insert(j);
             }
         }
     }
 
-
+    for(int i=0;i<matrix.size();i++){
+        for(int j=0;j<matrix[i].size();j++){
+            if(row.find(i)!=row.end() || col.find(j)!=col.end()){
+                matrix[i][j]=0;
+            }
+        }
     }
+
+    return;
+    }
+};
