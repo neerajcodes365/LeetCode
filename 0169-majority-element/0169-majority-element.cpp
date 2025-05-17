d247c3081a93da2c5feab179bb0eb4c50d92aac9
Neeraj J <neeraj2gokul4@gmail.com>
2025-05-17 16:44:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,3 +1,4 @@
+/*       NlogN 
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -17,5 +18,26 @@ public:
             }
         }
         return nums[j];
+    }
+};
+
+*/
+
+//O(N)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       // sort(nums.begin(),nums.end());
+       int count=0;
+       int leader;
+       for(int i=0;i<nums.size();i++){
+        if(count==0){
+            leader=nums[i];
+            count=1;
+        }
+        if(nums[i]==leader) count++;
+        else count--;
+       }
+       return leader;
     }
 };
