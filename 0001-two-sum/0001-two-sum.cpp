cada34bf6daf286a6a6b2aede33e298895f98184
Neeraj J <neeraj2gokul4@gmail.com>
2025-05-19 20:32:28 +0530

    Time: 5 ms (50.49%), Space: 16.1 MB (7.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,16 +3,18 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         vector<int>ans;
-        unordered_set<int,int>hash;
+        unordered_map<int,int>hash;
         for(int i=0;i<nums.size();i++){
-            hash.inset({nums[i],i});
+            hash.insert({nums[i],i});
         }
         for(int i=0;i<nums.size();i++){
-            int other=targert-nums[i];
-            if(hash.find(other)!=hash.end()){
+            int other=target-nums[i];
+            if(hash.find(other)!=hash.end() && hash[other]!=i){
                 ans.push_back(i);
-
+                ans.push_back(hash[other]);
+                return ans;
             }
         }
+        return ans;
     }
 };
