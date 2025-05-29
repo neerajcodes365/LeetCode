c33261ad04b84b28cfb0edc6a84f2eaad4d8dac0
Neeraj J <neeraj2gokul4@gmail.com>
2025-05-30 00:26:11 +0530

    Time: 1648 ms (5.03%), Space: 488.4 MB (5.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,54 +1,26 @@
-/*
-
 class Solution {
-public:
-   // void solve(vector<int>& candidates, int target, vector<vector<int>>& result, int sum, vector<int>& arr) {
-   void solve(vector<int>& candidates, int target, set<vector<int>>&result, int sum, vector<int>& arr) {
-        if (sum == target) {
-            vector<int>ar=arr;
-            sort(ar.begin(),ar.end());
-            // result.push_back(ar);
-            result.insert(ar);
-            return;
+    private:
+    void help(vector<int>& candi,set<vector<int>>&res,int count,int target,vector<int>arr){
+        if(count>target) return;
+        if(count==target){
+            sort(arr.begin(),arr.end());
+            res.insert(arr);
         }
-        if (sum > target) return;
-        for (int it : candidates) { 
-            arr.push_back(it);
-            solve(candidates, target, result, sum + it, arr);
+        for(int i=0;i<candi.size();i++){
+            //if()
+            arr.push_back(candi[i]);
+            count+=candi[i];
+            help(candi,res,count,target,arr);
             arr.pop_back();
+            count-=candi[i];
         }
     }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-       // vector<vector<int>> result;
-        set<vector<int>>result;
-        vector<int> arr;
-        solve(candidates, target, result, 0, arr);
-        return vector<vector<int>>(result.begin(),result.end());
-    }
-};
-
-*/
-class Solution {
 public:
-    void solve(vector<int>& candidates, int target, vector<vector<int>>& result, vector<int>& arr, int sum, int start) {
-        if (sum == target) {
-            result.push_back(arr);
-            return;
-        }
-        if (sum > target) return; 
-
-        for (int i = start; i < candidates.size(); i++) { 
-            arr.push_back(candidates[i]);
-            solve(candidates, target, result, arr, sum + candidates[i], i); 
-            arr.pop_back();
-        }
-    }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> result;
-        vector<int> arr;
-        solve(candidates, target, result, arr, 0, 0); 
+    vector<vector<int>> combinationSum(vector<int>& candi, int target) {
+        set<vector<int>>res;
+        vector<int>arr;
+        help(candi,res,0,target,arr);
+        vector<vector<int>>result(res.begin(),res.end());
         return result;
     }
 };
