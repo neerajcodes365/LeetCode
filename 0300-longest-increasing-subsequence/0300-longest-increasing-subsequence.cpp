0fbadb180d9cb432497096e400d2da0c4a3ad3d1
Neeraj J <neeraj2gokul4@gmail.com>
2025-06-22 17:54:23 +0530

    Time: 0 ms (100%), Space: 13.9 MB (99.17%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,42 @@
-class Solution {
+// class Solution {   cp3 solution 
+// public:
+//     int lengthOfLIS(vector<int>& arr) {
+//         int n=arr.size();
+//         vector<int>dp(n,0);
+//         dp[0]=1;
+//         int ans=1;
+//         for(int i=1;i<n;i++){
+//             int j=i-1;
+//             int maxi=0;
+//             while(j>=0){
+//                 if(arr[j]<arr[i]) maxi=max(dp[j],maxi);
+//                 j--;
+//             }
+//             dp[i]=maxi+1;
+//             ans=max(ans,dp[i]);
+//         }
+//         return ans;
+//     }
+// };
+
+
+class Solution {  //nlogn solution
 public:
     int lengthOfLIS(vector<int>& arr) {
         int n=arr.size();
-        vector<int>dp(n,0);
-        dp[0]=1;
-        int ans=1;
+        vector<int>dp;
+        dp.push_back(arr[0]);
+        int j=0;
         for(int i=1;i<n;i++){
-            int j=i-1;
-            int maxi=0;
-            while(j>=0){
-                if(arr[j]<arr[i]) maxi=max(dp[j],maxi);
-                j--;
+            if(arr[i]>dp.back()){
+                dp.push_back(arr[i]);
             }
-            dp[i]=maxi+1;
-            ans=max(ans,dp[i]);
+            else{
+                auto index=lower_bound(dp.begin(),dp.end(),arr[i]);
+                // dp[index]=arr[i];
+                *index=arr[i];
+            }
+
         }
-        return ans;
-    }
-};
+        return dp.size();
+    }};
