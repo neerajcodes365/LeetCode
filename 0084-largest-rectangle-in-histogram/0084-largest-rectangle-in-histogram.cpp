e3918e28a76a7f9f55d4f4550356b5203fa26cd6
Neeraj J <neeraj2gokul4@gmail.com>
2025-07-02 16:32:51 +0530

    Time: 15 ms (87.44%), Space: 85.7 MB (46.23%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& height) {
+        int n=height.size();
+        vector<int>front(n),back(n);
+        int mini=INT_MAX;
+        int index=0;
+        // for(int i=0;i<n;i++){  
+            /*this is wrong caz am making the smallest min till that index but we actually want the next small one from both side ,for this we use monotonic stack*/ 
+        //     // mini=min(mini,height[i]);
+        //     if(height[i]<mini){
+        //         index=i;
+        //         mini=height[i];
+        //     }
+        //     back[i]=index;
+        // }
+        // mini=INT_MAX;
+        // index=n-1;
+        // for(int i=n-1;i>=0;i--){
+        //    if(height[i]<mini){
+        //         index=i;
+        //         mini=height[i];
+        //     }
+        //     back[i]=index;
+        // }
+        stack<int>backi;
+        // ft.push(height[0]);
+        // backi.push(height[n-1]);
+
+        for(int i=0;i<n;i++){
+            while(!backi.empty() && height[backi.top()]>=height[i]){
+                backi.pop();
+            }
+            if(backi.empty()){
+                // back.push_back(-1);
+                back[i]=-1;
+            }else{
+                int num=backi.top();
+                // back.push_back(num);
+                back[i]=num;
+            }
+            backi.push(i);
+            
+        }
+
+        while(!backi.empty()) backi.pop();
+
+        for(int i=n-1;i>=0;i--){
+            while(!backi.empty() && height[backi.top()]>=height[i]){
+                backi.pop();
+            }
+            if(backi.empty()){
+                // front.push_back(-1);
+                front[i]=n;
+            }else{
+                int num=backi.top();
+                // front.push_back(num);
+                front[i]=num;
+            }
+            backi.push(i);
+        }
+
+int ans=0;
+    for(int i=0;i<n;i++){
+        int area=height[i]*(front[i]-back[i]-1);
+        ans=max(ans,area);
+    }
+return ans;
+    }
+};
