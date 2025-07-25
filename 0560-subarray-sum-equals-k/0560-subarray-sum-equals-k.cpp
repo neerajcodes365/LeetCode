438fdbe6a14a529a96b1f3f67be62de88b1ca8fd
Neeraj J <neeraj2gokul4@gmail.com>
2025-07-25 11:58:23 +0530

    Time: 2796 ms (5.12%), Space: 34.5 MB (79.39%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,24 +1,52 @@
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         // map<int,int>mp;//sum,index
+//         int sum=0;
+//         int n=nums.size();
+
+//         vector<int>prefix(n+1,0);
+
+//         for(int i=0;i<n;i++){
+//             prefix[i]=sum;
+//             sum+=nums[i];
+//         }
+
+//        prefix[n]=sum;
+//         int ans=0;
+//         for(int i=0;i<=n;i++){
+//             for(int j=i+1;j<=n;j++){
+//                 if((prefix[j]-prefix[i])==k) ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
+
 public:
+int ans;
+int n;
+
+    void help(vector<int>& nums, int k, int index, int sum) {
+        if (index >= n) return;
+
+        sum += nums[index];
+        if (sum == k) ans++;
+
+        help(nums, k, index + 1, sum); 
+    }
+
     int subarraySum(vector<int>& nums, int k) {
-        // map<int,int>mp;//sum,index
-        int sum=0;
-        int n=nums.size();
+        ans = 0;
+        n = nums.size();
 
-        vector<int>prefix(n+1,0);
-
-        for(int i=0;i<n;i++){
-            prefix[i]=sum;
-            sum+=nums[i];
+        for (int i = 0; i < n; i++) {
+            help(nums, k, i, 0);  
         }
 
-       prefix[n]=sum;
-        int ans=0;
-        for(int i=0;i<=n;i++){
-            for(int j=i+1;j<=n;j++){
-                if((prefix[j]-prefix[i])==k) ans++;
-            }
-        }
         return ans;
     }
 };
