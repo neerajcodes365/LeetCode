e8af4a3c7a6b5646ad73e317d0da9fe271f5fe3f
Neeraj J <neeraj2gokul4@gmail.com>
2025-07-25 11:37:40 +0530

    Time: 2164 ms (19.82%), Space: 36 MB (77.66%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,15 +4,18 @@ public:
         // map<int,int>mp;//sum,index
         int sum=0;
         int n=nums.size();
-        
-        vector<int>prefix(n,0);
-        for(int i-0;i<n;i++){
-            sum+=nums[i];
-            prefix[i]=sum;
-        }
-        int ans=0;
+
+        vector<int>prefix(n+1,0);
+
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;i++){
+            prefix[i]=sum;
+            sum+=nums[i];
+        }
+
+       prefix[n]=sum;
+        int ans=0;
+        for(int i=0;i<=n;i++){
+            for(int j=i+1;j<=n;j++){
                 if((prefix[j]-prefix[i])==k) ans++;
             }
         }
