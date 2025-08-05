7767d28403812783d65bd33317cb23595ee54f15
Neeraj J <neeraj2gokul4@gmail.com>
2025-08-05 20:11:50 +0530

    Time: 3 ms (89.69%), Space: 23.9 MB (57.28%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,3 +1,5 @@
+/*
+less space complexity
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& in) {
@@ -21,5 +23,26 @@ public:
         }
 
         return in;
+    }
+};
+*/
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.empty()) return {};
+
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (res.back()[1] >= intervals[i][0]) {
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            } else {
+                res.push_back(intervals[i]);
+            }
+        }
+
+        return res;
     }
 };
