f249fbe25e87379a5682bed04a77eb8a1411cb46
Neeraj J <neeraj2gokul4@gmail.com>
2025-08-06 18:52:28 +0530

    Time: 27 ms (5.11%), Space: 27 MB (5.01%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+        // map<char,int>
+        // bool ans;
+        int n;
+        bool help(string s,int i,int total,map<pair<int,int>, bool>&dp){
+            
+            if(i>=n && total==0) {
+                // ans=true;
+                // return dp[i][total]=1;
+                return dp[{i,total}]=1;
+                }
+            if(i>=n || total<0) return dp[{i,total}]=0;
+
+            if(dp.count({i,total})) return dp[{i,total}];
+
+            char ch=s[i];
+            // cout<<ch<<endl;
+            bool a=false;
+
+            if(ch=='(') a= help(s,i+1,total+1,dp);
+            else if(ch==')') a=help(s,i+1,total-1,dp);
+            else{
+                a= help(s,i+1,total,dp) || help(s,i+1,total+1,dp)|| help(s,i+1,total-1,dp);
+            }
+
+            return dp[{i,total}]=a;
+       }
+
+    bool checkValidString(string s) {
+
+        n=s.length();
+        // vector<vector<int>>dp(n,vector<int>(n,-1));
+        map<pair<int,int>, bool> dp;// i ,total, 
+        // ans=false;
+        return help(s,0,0,dp);
+        // return ans;
+    }
+};
