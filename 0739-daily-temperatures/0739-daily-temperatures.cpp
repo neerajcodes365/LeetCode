0ebffdf7e8c5d798cb34031041582f0a19529399
Neeraj J <neeraj2gokul4@gmail.com>
2025-10-05 10:50:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n=temp.size();
+        // while()
+        vector<int>ans(n);
+        stack<pair<int,int>>st;//num,index
+
+        for(int i=n-1;i>=0;i--){
+
+            while(!st.empty() && temp[i]>=st.top().first){
+                st.pop();
+            }
+
+            if(st.empty()){
+                st.push({temp[i],i});
+
+                ans[i]=0;
+                cout<<"i ="<<i<<" ,ans[i]= "<<ans[i]<<endl;
+                // continue;
+            }else{
+                ans[i]=(st.top().second - i);
+                cout<<"i ="<<i<<" ,ans[i]= "<<ans[i]<<endl;
+
+                st.push({temp[i],i});
+            }
+
+        }
+        return ans;
+        
+    }
+};
