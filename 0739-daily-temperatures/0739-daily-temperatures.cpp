6a09bf67c89bd22b83df5ac409dcc1b2bf1e9f76
Neeraj J <neeraj2gokul4@gmail.com>
2025-10-05 10:51:56 +0530

    Time: 34 ms (17.58%), Space: 102.9 MB (79.72%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,28 +3,30 @@ public:
     vector<int> dailyTemperatures(vector<int>& temp) {
         int n=temp.size();
         // while()
-        vector<int>ans(n);
+        vector<int>ans(n,0);
         stack<pair<int,int>>st;//num,index
 
         for(int i=n-1;i>=0;i--){
-
             while(!st.empty() && temp[i]>=st.top().first){
                 st.pop();
             }
+            // if(st.empty()){
+            //     st.push({temp[i],i});
 
-            if(st.empty()){
-                st.push({temp[i],i});
-
-                ans[i]=0;
-                cout<<"i ="<<i<<" ,ans[i]= "<<ans[i]<<endl;
-                // continue;
-            }else{
-                ans[i]=(st.top().second - i);
-                cout<<"i ="<<i<<" ,ans[i]= "<<ans[i]<<endl;
-
-                st.push({temp[i],i});
+            //     ans[i]=0;
+            //     cout<<"i ="<<i<<" ,ans[i]= "<<ans[i]<<endl;
+            //     // continue;
+            // }else{
+            //     ans[i]=(st.top().second - i);
+            //     cout<<"i ="<<i<<" ,ans[i]= "<<ans[i]<<endl;
+            //     st.push({temp[i],i});
+            // }
+            if(!st.empty()){
+                ans[i]=st.top().second-i;
             }
 
+            st.push({temp[i],i});
+            
         }
         return ans;
         
