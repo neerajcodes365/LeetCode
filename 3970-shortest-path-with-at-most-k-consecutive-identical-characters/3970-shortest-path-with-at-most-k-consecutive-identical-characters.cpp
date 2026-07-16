a967a3c5c6c6d03f7e4b28425c6e619587238ac4
Neeraj J <neeraj2gokul4@gmail.com>
2026-07-16 19:21:11 +0530

    Time: 170 ms (79.39%), Space: 274.6 MB (64.43%) - LeetHub

--- 3970-shortest-path-with-at-most-k-consecutive-identical-characters/3970-shortest-path-with-at-most-k-consecutive-identical-characters.cpp
+++ 3970-shortest-path-with-at-most-k-consecutive-identical-characters/3970-shortest-path-with-at-most-k-consecutive-identical-characters.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int shortestPath(int n, vector<vector<int>>& edges, string labels, int k) {
-        vector<vector<pair<int,int>>> adj(n); // size n, not m!
+        vector<vector<pair<int,int>>> adj(n); 
         for (auto& e : edges) {
             int node = e[0], neigh = e[1], wt = e[2];
             adj[node].push_back({neigh, wt});
         }
-
-        // dist[node][count] = best distance reaching node with `count`
-        // consecutive same labels ending at node
         const int INF = INT_MAX;
-        vector<vector<int>> dist(n, vector<int>(k + 1, INF));
-
+        vector<vector<int>> dist(n, vector<int>(k + 1, INF));//this was the thing i missed .
         // pq: {distance, node, count}
         priority_queue<
             tuple<int,int,int>,
@@ -26,16 +22,14 @@ public:
             auto [d, node, cnt] = pq.top();
             pq.pop();
 
-            if (d > dist[node][cnt]) continue; // stale entry
-            // if (node == n - 1) return d;        // first time we pop n-1 is optimal
-
+            if (d > dist[node][cnt]) continue; 
+            if (node == n - 1) return d;        
             for (auto& [neigh, wt] : adj[node]) {
                 int newCnt;
                 if (labels[neigh] == labels[node]) newCnt = cnt + 1;
                 else newCnt = 1;
 
-                if (newCnt > k) continue; // exceeds allowed streak, don't push
-
+                if (newCnt > k) continue; 
                 int nd = d + wt;
                 if (nd < dist[neigh][newCnt]) {
                     dist[neigh][newCnt] = nd;
