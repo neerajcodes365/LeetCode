9e31aae20d28fc9cced0b40a798b33cb9eb3d1fe
Neeraj J <neeraj2gokul4@gmail.com>
2026-08-04 03:48:47 +0530

    Time: 0 ms (100%), Space: 12.8 MB (6.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int maxi;
-    // max=-1;
-    void backtrack(vector<int>&nums,int index,int sum){
-        if(sum>maxi)maxi=sum;
-        if(index>=nums.size()) return;
-
-        for(int i=index;i<nums.size();i++){
-            // if(index==i-1) continue;
-            sum+=nums[i];
-            backtrack(nums,i+2,sum);
-            sum-=nums[i];
-        }
+    int help(vector<int>nums,int i,int n,vector<int>&dp){
+        if(i>=n)return 0;
+        if(dp[i]!=-1)return dp[i];
+        int skip=help(nums,i+1,n,dp);
+        int pick=nums[i]+help(nums,i+2,n,dp);
+        return dp[i]=max(skip,pick);
     }
     int rob(vector<int>& nums) {
-        int sum=0;
-        maxi=-1;
-        backtrack(nums,0,sum);
-        return maxi;
+        int n=nums.size();
+        vector<int>dp(n+1,-1);
+        return help(nums,0,n,dp);
     }
 };
