8d766d5eb6656dd367d1bd5b38082b4bace1f046
Neeraj J <neeraj2gokul4@gmail.com>
2026-08-05 03:39:42 +0530

    Time: 55 ms (18.68%), Space: 49.9 MB (19.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-  unsigned  long long int help(vector<int>&coins,int amount,long long int  count,int idex,vector<vector<long long>>&dp){
+    int help(vector<int>&coins,int index,int amount,int sum,vector<vector<int>>&dp){
+        if(index<0 || sum>amount)return INT_MAX;
+        if(sum==amount) return 0;
+        if(dp[index][sum]!=-1)return dp[index][sum];
+        // int picknext =help(coins,index-1,amount,sum+coins[index],ans+1,dp);
+        int picksame=help(coins,index,amount,sum+coins[index],dp);
+        if(picksame!=INT_MAX) picksame++;
+        int skip=help(coins,index-1,amount,sum,dp);
+        // if(skip!=INT_MAX) skip++;
+        // return dp[index][sum]=min({picknext,picksame,skip});
+        return dp[index][sum]=min(picksame,skip);
 
-        if(count==amount) return 0;
-        if(count>amount || idex>=coins.size()) return LLONG_MAX;
-            if(dp[count][idex]!=-1)return dp[count][idex];
-
-
-
-        // int pick=
-        unsigned long long int pick=help(coins,amount,count+coins[idex],idex,dp);
-        if(pick!=LLONG_MAX) pick+=1;
-
-        // int notpick;
-       unsigned  long long int notpick=help(coins,amount,count,idex+1,dp);
-
-        return dp[count][idex]=min(pick,notpick);
     }
     int coinChange(vector<int>& coins, int amount) {
-        // mini=-1;
-vector<vector<long long>> dp(amount + 1, vector<long long>(coins.size(), -1));
-      unsigned long long int  mini=help(coins,amount,0,0,dp);
-        return mini==LLONG_MAX?-1:mini;
+        sort(coins.begin(),coins.end());
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
+        int a= help(coins,coins.size()-1,amount,0,dp);
+        return a==INT_MAX?-1:a;
     }
 };
