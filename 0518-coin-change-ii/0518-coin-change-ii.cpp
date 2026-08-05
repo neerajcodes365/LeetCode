28193ace2eb8cd4ece2e292d8b14a4d73e6d98c3
Neeraj J <neeraj2gokul4@gmail.com>
2026-08-05 16:55:52 +0530

    Time: 35 ms (41.43%), Space: 36.8 MB (39.52%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<unsigned long long>>dp(n+1,vector<unsigned long long>(amount+1,0));
-        dp[0][0] = 1;
+    int help(int amount,vector<int>&coins,int index,int sum,vector<vector<int>>&dp){
+        if(sum==amount)return 1;
+        if(sum>amount || index<0)return 0;
+        if(dp[index][sum]!=-1) return dp[index][sum];
+        int notskip=help(amount,coins,index,sum+coins[index],dp);
+        int skip=help(amount,coins,index-1,sum,dp);
+        return dp[index][sum]=skip+notskip;
+    }
 
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<amount+1;j++){
-       unsigned  long long exclude=dp[i-1][j];
-     unsigned  long long include=0;
-                if(coins[i-1]<=j) include=dp[i][j-coins[i-1]];
-                dp[i][j]=exclude+include;
-            }
-        }
-return dp[n][amount];
+    int change(int amount, vector<int>& coins) {
+        int sum=0;
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
+       return help(amount,coins,coins.size()-1,sum,dp);
     }
 };
